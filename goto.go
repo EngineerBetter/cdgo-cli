06d5fb0e62e042d9816c059672b5452bf8ae3f61
Daniel Jones <daniel.jones@engineerbetter.com>
2016-04-19 11:07:22 +0100

    goto executable and simple test

---
+++ goto.go
@@ -0,0 +1,32 @@
+package main
+
+import "os"
+import "path/filepath"
+import "fmt"
+
+func main() {
+	gopath := os.Getenv("GOPATH")
+
+	if gopath == "" {
+		fmt.Println("GOPATH is not set")
+		os.Exit(1)
+	}
+
+	haystack := filepath.Join(gopath, "src")
+
+	if len(os.Args) < 2 {
+		fmt.Println("directory to look for was not specified")
+		os.Exit(1)
+	}
+	needle := os.Args[1]
+
+	finder := new(RecursiveFinder)
+	result, err := finder.Find(needle, haystack)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(result)
+}
---
+++ goto_test.go
@@ -0,0 +1,32 @@
+package main_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+	. "github.com/onsi/gomega/gexec"
+
+	"os/exec"
+)
+
+var _ = Describe("goto", func() {
+	var cliPath string
+
+	BeforeSuite(func() {
+		var err error
+		cliPath, err = Build("github.com/EngineerBetter/goto")
+		Ω(err).ShouldNot(HaveOccurred())
+	})
+
+	AfterSuite(func() {
+		CleanupBuildArtifacts()
+	})
+
+	It("requires an argument", func() {
+		command := exec.Command(cliPath)
+		session, err := Start(command, GinkgoWriter, GinkgoWriter)
+		Ω(err).ShouldNot(HaveOccurred())
+		Eventually(session).Should(Exit(1))
+		Ω(session.Out).Should(Say("directory to look for was not specified"))
+	})
+})

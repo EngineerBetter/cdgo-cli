9b4fdb5688d8baad9c51867a6f61a068d1c44962
Daniel Jones <daniel.jones@engineerbetter.com>
2016-04-19 10:04:11 +0100

    use filepath.Walk to recurse directories

--- finder.go
+++ finder.go
@@ -2,6 +2,7 @@ package main
 
 import "errors"
 import "os"
+import "path/filepath"
 
 type DirectoryFinder interface {
 	Find(directory string, in string) (string, error)
@@ -9,14 +10,27 @@ type DirectoryFinder interface {
 
 type RecursiveFinder struct{}
 
-func (*RecursiveFinder) Find(needle string, haystack string) (result string, err error) {
-	info, err := os.Stat(haystack)
+func (*RecursiveFinder) Find(needle string, haystack string) (result string, errOut error) {
+	fi, err := os.Lstat(haystack)
 
 	if err != nil {
-		return result, err
-	} else if !info.IsDir() {
-		return result, errors.New("Path was not a directory")
+		return result, errors.New(haystack + " did not exist")
+	} else if !fi.IsDir() {
+		return result, errors.New(haystack + " is not a directory")
 	}
 
-	return "foo", nil
+	filepath.Walk(haystack, func(path string, fi os.FileInfo, errIn error) (errOut error) {
+		if fi.Name() == needle {
+			result = path
+			return filepath.SkipDir
+		}
+
+		return
+	})
+
+	if result == "" {
+		errOut = errors.New(needle + "not found in " + haystack)
+	}
+
+	return
 }
